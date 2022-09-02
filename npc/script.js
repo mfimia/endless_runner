f41b008c5ed240a971cd3adeca64e3a208595093
Martin <martin@clareandme.com>
2022-09-02 18:00:28 +0200

    add sun enemy class and movement patterns

--- npc/script.js
+++ npc/script.js
@@ -2,8 +2,8 @@
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 500
-const CANVAS_HEIGHT = canvas.height = 1000
-const enemyAmount = 200
+const CANVAS_HEIGHT = canvas.height = 800
+const enemyAmount = 20
 const enemiesArray = []
 const enemySizeRatio = 2.5
 
@@ -98,10 +98,46 @@ class GhostEnemy {
   }
 }
 
+class SunEnemy {
+  constructor () {
+    this.image = new Image()
+    this.image.src = './assets/enemy4.png'
+    this.speed = Math.random() * 4 + 1
+    this.spriteWidth = 213
+    this.spriteHeight = 213
+    this.width = this.spriteWidth / enemySizeRatio
+    this.height = this.spriteHeight / enemySizeRatio
+    this.x = Math.random() * (canvas.width - this.width)
+    this.y = Math.random() * (canvas.height - this.height)
+    this.newX = Math.random() * (canvas.width - this.width)
+    this.newY = Math.random() * (canvas.height - this.height) // gets random target within canvas
+    this.frame = 0
+    this.flapSpeed = Math.floor(Math.random() * 10 + 1)
+    this.interval = Math.floor(Math.random() * 200 + 50)
+  }
+  update () {
+    if (gameFrame % this.interval === 0) { // get a new target after some time
+      this.newX = Math.random() * (canvas.width - this.width)
+      this.newY = Math.random() * (canvas.height - this.height)
+    }
+    let dx = this.x - this.newX // calculates distance to target
+    let dy = this.y - this.newY
+    this.x -= dx / 70 // travels to target
+    this.y -= dy / 70
+    if (gameFrame % this.flapSpeed === 0)
+      this.frame > 4 ? this.frame = 0 : this.frame++ // animates sprite frames
+  }
+  draw () {
+    ctx.drawImage(this.image,
+      this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight,
+      this.x, this.y, this.width, this.height)
+  }
+}
+
 for (let i = 0; i < enemyAmount; i++) {
   // enemiesArray.push(new BatEnemy())
   // enemiesArray.push(new BirdEnemy())
-  enemiesArray.push(new GhostEnemy())
+  enemiesArray.push(new SunEnemy())
 }
 
 const animate = () => {
