99aff79fdcec8d156c3ec893c393a63f09897fa8
Martin <martin@clareandme.com>
2022-09-14 17:49:47 +0200

    calculate deltatime to serve frame equally in all machines

--- state_management/script.js
+++ state_management/script.js
@@ -1,7 +1,8 @@
+/** @type {HTMLCanvasElement} */
 import Player from './player.js'
 import InputHander from './input.js'
+import { drawStatusText } from './utils.js'
 
-/** @type {HTMLCanvasElement} */
 
 window.addEventListener('load', () => {
   const loading = document.getElementById('loading')
@@ -12,12 +13,18 @@ window.addEventListener('load', () => {
   canvas.height = window.innerHeight
 
   const player = new Player(canvas.width, canvas.height)
-  player.draw(ctx)
   const input = new InputHander()
 
-  const animate = () => {
+  let lastTime = 0
+  const animate = (timestamp) => {
+    const deltaTime = timestamp - lastTime
+    lastTime = timestamp
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    player.update(input.lastKey) // #1 - update the player with input
+    player.draw(ctx, deltaTime)
+    drawStatusText(ctx, input, player)
     requestAnimationFrame(animate)
   }
 
-  animate()
+  animate(0)
 })
