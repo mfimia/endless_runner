cf5f034a6b5e9b25f3af1837749e162ac88b5944
Martin <martin@clareandme.com>
2022-09-16 11:43:29 +0200

    define game controls

--- endless_runner/input.js
+++ endless_runner/input.js
@@ -3,11 +3,23 @@ export default class InputHandler {
     this.keys = []
     window.addEventListener('keydown', e => {
       const { key } = e
-      if ((key === 'ArrowDown' || key === 'ArrowUp') && this.keys.indexOf(key) === -1) this.keys.push(key)
+      if (
+        (
+          key === 'ArrowDown' ||
+          key === 'ArrowUp' ||
+          key === 'ArrowLeft' ||
+          key === 'ArrowRight' ||
+          key === 'Meta'
+        )
+        && this.keys.indexOf(key) === -1) this.keys.push(key)
     })
     window.addEventListener('keyup', e => {
       const { key } = e
-      if (key === 'ArrowDown') this.keys.splice(this.keys.indexOf(key), 1)
+      if (key === 'ArrowDown' ||
+        key === 'ArrowUp' ||
+        key === 'ArrowLeft' ||
+        key === 'ArrowRight' ||
+        key === 'Meta') this.keys.splice(this.keys.indexOf(key), 1)
     })
   }
 }
