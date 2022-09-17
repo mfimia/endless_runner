c8bb2ef1c67a1f87c06eb064dbbb0b6583669491
Martin <martin@clareandme.com>
2022-09-17 11:38:48 +0200

    add recommended settings. set up delta time logic

--- endless_runner/main.js
+++ endless_runner/main.js
@@ -1,5 +1,7 @@
+import { BOUNDARIES, GAME_SPEED, RECOMMENDED_SETTINGS } from "./constants.js"
 import InputHandler from "./input.js"
 import { Player } from "./player.js"
+import { Background } from './background.js'
 
 window.addEventListener('load', () => {
   const canvas = document.getElementById('canvas1')
@@ -11,25 +13,34 @@ window.addEventListener('load', () => {
     constructor (width, height) {
       this.width = width
       this.height = height
+      this.groundMargin = BOUNDARIES.GROUND
+      this.speed = GAME_SPEED.PAUSED // pixels per frame
+      this.maxSpeed = RECOMMENDED_SETTINGS.MAX_SPEED
+      this.background = new Background(this)
       this.player = new Player(this)
       this.input = new InputHandler()
     }
-    update () {
-      this.player.update(this.input.keys)
+    update (deltaTime) {
+      this.background.update()
+      this.player.update(this.input.keys, deltaTime)
     }
     draw (context) {
+      this.background.draw(context) // background drawn behind player
       this.player.draw(context)
     }
   }
 
   const game = new Game(canvas.width, canvas.height)
 
+  let lastTime = 0
 
-  const animate = () => {
+  const animate = (timestamp) => {
+    const deltaTime = timestamp - lastTime // how long it takes to serve a frame
+    lastTime = timestamp
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     game.draw(ctx)
-    game.update()
+    game.update(deltaTime)
     requestAnimationFrame(animate)
   }
-  animate()
+  animate(0)
 })
